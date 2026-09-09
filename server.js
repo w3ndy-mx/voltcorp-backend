a96b684f9e1c5d93b5c77866e7602aa8ba1df04b
w3ndy-mx <wendytestaburguer651@gmail.com>
2026-09-08 20:12:03 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,13 +5,4 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Tus rutas aquí...
-app.get('/', (req, res) => {
-    res.send('Servidor activo');
-});
-
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
+const OLLAMA_URL = process.env.OLLAMA_URL
